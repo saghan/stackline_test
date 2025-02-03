5e4e31f0bfd61c78693e766c76fbcd4dc61902d2
saghan <saghan99@gmail.com>
2025-02-03 12:37:54 -0800

    added logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 import Item from './components/Item';
 import data from './data.json';
+import stacklineLogo from './assets/images/stackline_logo.svg'
 
 function App() {
   return (
     <div className="App">
+      <nav className="navbar">
+        <img src={stacklineLogo} alt="Stackline Logo" className="navbar-logo" />
+      </nav>
       <Item data={data[0]} />
     </div>
   );
