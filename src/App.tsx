2a02a5184357475aa6784506b8991a79a6baefa2
saghan <saghan99@gmail.com>
2025-02-03 11:29:28 -0800

    misc

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.css';
+import Item from './components/Item';
+import data from './data.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Item data={data[0]} />
     </div>
   );
 }
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -4,12 +4,13 @@ import './Item.css';
 interface ItemData {
   id: string;
   title: string;
+  image: string;
   subtitle: string;
   brand: string;
   reviews: {
     customer: string;
     review: string;
-    rating: number;
+    score: number;
   }[];
   retailer: string;
   details: string[];
@@ -24,13 +25,41 @@ interface ItemData {
 }
 
 interface ItemProps {
-  data: any; // We'll type this properly once we see data.json
+  data: ItemData;
 }
 
 const Item: React.FC<ItemProps> = ({ data }) => {
   return (
     <div className="item">
-      {/* We'll add the appropriate JSX once we see your data structure */}
+      <div className="item-layout">
+        <div className="item-left-pane">
+          <div className="item-card">
+            <img src={data.image} alt={data.title} className="item-image" />
+            <div className="item-card-content">
+              <h2>{data.title}</h2>
+              <h3>{data.subtitle}</h3>
+              <div className="tags">
+                {data.tags.map((tag, index) => (
+                  <span key={index} className="tag">{tag}</span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="item-right-pane">
+          <div className="item-info">
+            <p>Retailer: {data.retailer}</p>
+          </div>
+          <div className="details">
+            <h4>Details:</h4>
+            <ul>
+              {data.details.map((detail, index) => (
+                <li key={index}>{detail}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
